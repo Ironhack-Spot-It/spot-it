d9f5c9669afca96966fb02f356e7a599c31af01a
Paula Quitián <pquitian@idealista.com>
2018-07-22 17:12:47 +0200

    feat: user model

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    spotifyId = String,
+    name = String, 
+    email = {
+        type: String, 
+        unique: true, 
+        required: true
+    }, 
+    image = String, 
+    privateDates = Object
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User; 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
+
+router.post('/auth'), passport.authenticate('spotify-auth', {scope: ['user-read-email', 'user-read-private', 'user-read-recently-played', 'user-top-read', 'user-read-birthdate', ] });
 router.get('/spotify/callback', authController.loginCallback)
