ad713d336ff36397017435a21b962d5f12c70c4d
Paula Quitián <pquitian@idealista.com>
2018-07-23 20:01:42 +0200

    bug: fixing problems with routes and spotify strategy

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 
-router.post('/'), passport.authenticate('spotify-auth', {scope: ['user-read-email', 'user-read-private', 'user-read-recently-played', 'user-top-read', 'user-read-birthdate'] });
+router.post('/', passport.authenticate('spotify', {scope: ['user-read-email', 'user-read-private', 'user-read-recently-played', 'user-top-read', 'user-read-birthdate'] }));
 
 router.get('/spotify/cb', authController.loginCallback);
 
