ff42c3ae2f9788e1bf5b1b4e3cb5c527bd4455b6
Estibaliz Fernandez <estibalizfpalacios@gmail.com>
2018-08-03 18:06:09 +0200

    first deploy

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const DB_NAME = 'spot-it';
-const DB_URI = `mongodb://localhost:27017/${DB_NAME}`;
+const DB_URI = `mongodb://heroku_94z2d8n7:p470g8egiv8lou24u04d3jji1d@ds211592.mlab.com:11592/heroku_94z2d8n7`;
 
-mongoose.connect(DB_URI)
+
+mongoose.connect(process.env.DB_URI)
     .then(()=> 
         console.info(`Connected to ${DB_NAME} database`)
     )
