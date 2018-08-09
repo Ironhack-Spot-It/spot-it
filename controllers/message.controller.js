d1be2da2292cd99e50b10e24e4aad955e2938b1f
pquitian <pqhernandez@gmail.com>
2018-08-09 08:48:13 +0200

    ui changes

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Msg = require('../models/message.model');
+const User = require('../models/user.model');
 const moment = require('moment');
 
 
@@ -23,6 +24,15 @@ module.exports.showInbox = (req, res, next) => {
                 let senders = allMessages.map((msg)=> msg.from);
                 let allSenders = senders.filter((item, pos) =>
                     senders.indexOf(item) == pos);
+
+                let prueba = allSenders.map((sender) => {
+                    return User.find({name: sender}, {image: 1, name: 1})
+                                .then(s => {
+                                    console.log(s);
+                                })
+                                .catch(error => next(error));
+                });
+                console.log(prueba);
     
                 res.render('users/inbox', 
                 { 
@@ -43,19 +53,13 @@ module.exports.showInbox = (req, res, next) => {
 
 module.exports.showMessage = (req, res, next) => {
     if (req.params.name === req.user.name) {
-    // console.log('params: ', req.params);   
-    // console.log('user', req.user);
+    
     Msg.find({$or: [{ from: req.params.sender, to: req.user.name}, {from: req.user.name, to: req.params.sender}]})
         .then (messages => {
-            // let sentTime = messages.map((messages) => { 
-            //    let ms = messages.createdAt;
-            //    return moment().to(ms);
-            // });
 
-            // console.log('TIME COHONE: ', sentTime);
+            
             res.render('users/message', {
-                message: messages, 
-                // time: sentTime
+                message: messages
             })
             
         })
